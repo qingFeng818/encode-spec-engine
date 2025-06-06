035618a7748bc8c1510f4579799b86580cb8aedc
qingfeng818 <dduuyyuu@qq.com>
2025-06-06 20:32:08 +0800

    text: text yml

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+set -e
+
+
+push_addr=$(git remote get-url --push origin)
+commit_info=$(git describe --all --always --long)
+dist_path="docs/.vuepress/dist"
+push_branch="gh-pages"
+
+npm run docs:build
+
+cd "$dist_path" && git config core.filemode false 
+
+git init
+git add -A
+git commit -m "deploy: $commit_info"
+
+git push -f "$push_addr" HEAD:"$push_branch"
+
+cd -
+
+rm -rf "$dist_path"
